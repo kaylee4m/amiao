7c79fa357c277fe677c34dec4133e87840d04a2b
kaylee4m <nning9@yeah.net>
2021-01-15 16:44:32 +0800

    update

--- web_admin-master/info/modules/camera/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# 创建蓝图对象
-from flask import Blueprint
-
-camera_blu = Blueprint('camera', __name__)
-
-from . import views
--- web_admin-master/info/modules/connections/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# 创建蓝图对象
-from flask import Blueprint
-
-connection_blu = Blueprint('connection', __name__)
-
-from . import views
